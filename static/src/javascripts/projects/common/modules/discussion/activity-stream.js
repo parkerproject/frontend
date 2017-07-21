9fe19e605bafe5b17c72c4463e8992d754079c0c
GHaberis <george.haberis@theguardian.com>
2017-07-21 16:23:05 +0100

    es6 convert acitivity stream

--- static/src/javascripts/projects/common/modules/discussion/activity-stream.js
+++ static/src/javascripts/projects/common/modules/discussion/activity-stream.js
@@ -1,112 +1,177 @@
+// @flow
 import bonzo from 'bonzo';
 import bean from 'bean';
 import $ from 'lib/$';
-import url from 'lib/url';
-import component from 'common/modules/component';
+import {
+    supportsPushState,
+    getUrlVars,
+    constructQuery,
+    replaceQueryString,
+    pushQueryString,
+} from 'lib/url';
+import Component from 'common/modules/component';
 import discussionApi from 'common/modules/discussion/api';
 
-function ActivityStream(opts) {
-    this.setOptions(opts);
-}
-component.define(ActivityStream);
-ActivityStream.prototype.endpoint = '/discussion/profile/:userId/:streamType.json?page=:page';
-ActivityStream.prototype.componentClass = 'activity-stream';
+const pagination = activityStream => {
+    bean.on(
+        activityStream.elem,
+        'click',
+        '.js-activity-stream-page-change',
+        e => {
+            const page = e.currentTarget.getAttribute('data-page');
 
-ActivityStream.prototype.defaultOptions = {
-    page: 1,
-    streamType: 'discussions',
-    userId: null
-};
-ActivityStream.prototype.ready = function() {
-    this.removeState('loading');
-    this.on('click', '.js-disc-recommend-comment', this.recommendComment);
-    $('.js-disc-recommend-comment').addClass('disc-comment__recommend--open');
+            e.preventDefault();
 
-    window.onpopstate = function(event) {
-        if (url.hasHistorySupport) {
-            this.applyState(event.state.resp.html, event.state.streamType);
+            activityStream.change({
+                page,
+            });
         }
-    }.bind(this);
-
-    pagination(this);
-};
-ActivityStream.prototype.recommendComment = function(e) {
-    var el = e.currentTarget;
-    discussionApi.recommendComment(el.getAttribute('data-comment-id'));
-    bonzo(el).addClass('disc-comment__recommend--active');
-    $('.js-disc-recommend-count', el).each(function(countEl) {
-        countEl.innerHTML = parseInt(countEl.innerHTML, 10) + 1;
-    });
-};
-ActivityStream.prototype.change = function(opts) {
-    this.setOptions(opts);
-    return this._fetch();
-};
-ActivityStream.prototype.fetched = function(resp) {
-    this.applyState(resp.html, this.options.streamType);
-    this.updateHistory(resp);
-};
-ActivityStream.prototype.applyState = function(html, streamType) {
-    // update display
-    var $el = bonzo(this.elem).empty();
-    this.setState('loading');
-    $.create(html).each(function(el) {
-        $el.html($(el).html()).attr({
-            'class': el.className
-        });
-    });
-    this.removeState('loading');
-
-    var activeTab = $('.tabs__tab--selected');
-    if (activeTab.data('stream-type') !== streamType) {
-        selectTab(streamType === 'comments' ? 'discussions' : streamType);
-    }
-
-    // update opts
-    this.options.streamType = streamType;
-};
-ActivityStream.prototype.updateHistory = function(resp) {
-    var page = this.options.page;
-    var pageParam = url.getUrlVars().page;
-    var streamType = this.options.streamType !== 'discussions' ? '/' + this.options.streamType : '';
-    var qs = '/user/id/' + this.options.userId + streamType + '?' + url.constructQuery({
-        page: page
-    });
-    var state = {
-        resp: resp,
-        streamType: this.options.streamType
-    };
-    var params = {
-        querystring: qs,
-        state: state
-    };
-
-    if (typeof pageParam === 'undefined') { // If first load and without page param, add it and overwrite history
-        url.replaceQueryString(params);
-    } else {
-        url.pushQueryString(params);
-    }
+    );
 };
 
-function pagination(activityStream) {
-    bean.on(activityStream.elem, 'click', '.js-activity-stream-page-change', function(e) {
-        var page = e.currentTarget.getAttribute('data-page');
-        e.preventDefault();
+const selectTab = streamType => {
+    const tabLinks = document.getElementsByClassName(
+        'js-activity-stream-change'
+    )[0];
 
-        activityStream.change({
-            page: page
-        });
-    });
-}
-
-function selectTab(streamType) {
     // Blur so that when pressing forward/back the focus is not retained on
     // the old tab Note, without the focus first, the blur doesn't seem to
     // work for some reason
-    $('.js-activity-stream-change').focus().blur();
+    if (tabLinks) {
+        tabLinks.focus();
+        tabLinks.blur();
+    }
 
-    $('.tabs__tab--selected').removeClass('tabs__tab--selected');
-    bonzo($('a[data-stream-type=' + streamType + ']')).parent().addClass('tabs__tab--selected');
+    const selectedTab = document.getElementsByClassName(
+        'js-activity-stream-change'
+    )[0];
+
+    if (selectedTab) {
+        selectedTab.classList.remove('tabs__tab--selected');
+    }
+
+    const selectedTabLink = document.querySelector(
+        `a[data-stream-type=${streamType}]`
+    );
+
+    if (selectedTabLink) {
+        const selectedTabLinkParent = selectedTabLink.parentNode;
+
+        if (selectedTabLinkParent) {
+            selectedTabLinkParent.classList.add('tabs__tab--selected');
+        }
+    }
+};
+
+const recommendComment = e => {
+    const el = e.currentTarget;
+
+    discussionApi.recommendComment(el.getAttribute('data-comment-id'));
+
+    el.classList.add('disc-comment__recommend--active');
+
+    const counters = [
+        ...document.getElementsByClassName('js-disc-recommend-count'),
+    ];
+
+    counters.forEach(counter => {
+        counter.innerHTML = parseInt(counter.innerHTML, 10) + 1;
+    });
+};
+
+class ActivityStream extends Component {
+    constructor(opts) {
+        super();
+
+        this.endpoint =
+            '/discussion/profile/:userId/:streamType.json?page=:page';
+        this.componentClass = 'activity-stream';
+        this.defaultOptions = {
+            page: 1,
+            streamType: 'discussions',
+            userId: null,
+        };
+        this.setOptions(opts);
+    }
+
+    ready() {
+        this.removeState('loading');
+        this.on('click', '.js-disc-recommend-comment', recommendComment);
+
+        const counters = [
+            ...document.getElementsByClassName('js-disc-recommend-count'),
+        ];
+
+        counters.forEach(counter => {
+            counter.classList.addClass('disc-comment__recommend--open');
+        });
+
+        window.onpopstate = event => {
+            if (supportsPushState) {
+                this.applyState(event.state.resp.html, event.state.streamType);
+            }
+        };
+
+        pagination(this);
+    }
+
+    change(opts) {
+        this.setOptions(opts);
+        return this.fetch();
+    }
+
+    fetched(resp) {
+        this.applyState(resp.html, this.options.streamType);
+        this.updateHistory(resp);
+    }
+
+    applyState(html, streamType) {
+        // update display
+        const $el = bonzo(this.elem).empty();
+        this.setState('loading');
+        $.create(html).each(el => {
+            $el.html($(el).html()).attr({
+                class: el.className,
+            });
+        });
+        this.removeState('loading');
+
+        const activeTab = $('.tabs__tab--selected');
+        if (activeTab.data('stream-type') !== streamType) {
+            selectTab(streamType === 'comments' ? 'discussions' : streamType);
+        }
+
+        // update opts
+        this.options.streamType = streamType;
+    }
+
+    updateHistory(resp) {
+        const page = this.options.page;
+        const pageParam = getUrlVars().page;
+        const streamType =
+            this.options.streamType !== 'discussions'
+                ? `/${this.options.streamType}`
+                : '';
+        const qs = `/user/id/${this.options
+            .userId}${streamType}?${constructQuery({
+            page,
+        })}`;
+        const state = {
+            resp,
+            streamType: this.options.streamType,
+        };
+        const params = {
+            querystring: qs,
+            state,
+        };
+
+        if (typeof pageParam === 'undefined') {
+            // If first load and without page param, add it and overwrite history
+            replaceQueryString(params);
+        } else {
+            pushQueryString(params);
+        }
+    }
 }
 
-export default ActivityStream;
+export { ActivityStream };
